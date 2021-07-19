89c84622e8f27de42c1b196abfed3d3eef2203c1
Miguel Cabral <78111674+Miguel-sdj@users.noreply.github.com>
2021-07-19 19:53:56 -0300

    Added some error handling

--- unstable.py
+++ unstable.py
@@ -18,7 +18,7 @@ def animate():
         sys.stdout.write('\rCarregando ' + c)
         sys.stdout.flush()
         time.sleep(0.1)
-    sys.stdout.write('\rTerminei, Baitola!')
+    sys.stdout.write('\rDownload Finalizado!')
     if is_movie == True:
         print("\nSeu vídeo se encontra no diretório: {}\Youtube_Downloader\Vídeos".format(os.getcwd()))
     else:
@@ -28,54 +28,79 @@ def animate():
 
 t = threading.Thread(target=animate)
 print('*'*100)
-link_video = str(input("Digite o link do video: "))
-if link_video == "link":
-    links_separados = separate_phrase('link.txt')
-    is_link = True
-else:
-    is_link = False
+
+link_is_valid = False
+while link_is_valid == False:
+    link_video = input("Digite o link do video: ")
+    link_video = str(link_video)
+    if link_video[:24] == "https://www.youtube.com/":
+        link_is_valid = True
+        is_link = False
+
+    elif link_video[:17] == "https://youtu.be/":
+        link_is_valid = True
+        is_link = False
+    
+
+    elif link_video == "link":
+        links_separados = separate_phrase('link.txt')
+        is_link = True
+        link_is_valid = True
+
+    else:
+        print("Link Inválido!")
+        link_is_valid = False
+        is_link = False
 
 try:
-    if is_link == False:
-        video = YouTube(link_video)
-        print(f'Título do vídeo: {video.title}')
-        print(f'Duração: {video.length / 60:.2f} minutos')
-        print(f'Número de views: {video.views}\n')
-        
-        escolha = int(input("1 - para baixar como vídeo\n2 - para baixar a música\nEscolha: "))
-        if escolha == 1:
-            is_movie = True
-            video = video.streams.get_highest_resolution()
-            print('Para que o vídeo tenha som e imagem é necessário baixar na resolução: {}'.format(video.resolution))
-            video.download('Youtube_Downloader/Vídeos')
-            t.start()
-        elif escolha == 2:
-            is_movie = False
-            t.start()
-            musica = YouTube(link_video).streams.filter(only_audio=True).first().download('Youtube_Downloader/Músicas')
-            base, ext = os.path.splitext(musica)
-            new_file = base + '.mp3'
-            os.rename(musica, new_file)
+    if link_is_valid:
+        try:
+            video_error = False
+            if is_link == False:
+                video = YouTube(link_video)
+                print(f'Título do vídeo: {video.title}')
+                print(f'Duração: {video.length / 60:.2f} minutos')
+                print(f'Número de views: {video.views}\n')
+
+        except:
+            print("Vídeo indisponível!")
+            video_error = True
+
+        if is_link == False:
+            escolha = int(input("1 - para baixar como vídeo\n2 - para baixar a música\nEscolha: "))
+            if escolha == 1:
+                is_movie = True
+                video = video.streams.get_highest_resolution()
+                print('Para que o vídeo tenha som e imagem é necessário baixar na resolução: {}'.format(video.resolution))
+                video.download('Youtube_Downloader/Vídeos')
+                t.start()
+
+            elif escolha == 2:
+                is_movie = False
+                t.start()
+                musica = YouTube(link_video).streams.filter(only_audio=True).first().download('Youtube_Downloader/Músicas')
+                base, ext = os.path.splitext(musica)
+                new_file = base + '.mp3'
+                os.rename(musica, new_file)
+
+            else:
+                print("escolhe um número válido")
+
         else:
-            print("escolhe um número válido")
-    else:
-        for i in range(len(links_separados)):
-            nome = YouTube(links_separados[i])
-            print("Iniciando download da música {}/{}: {}".format(i+1, len(links_separados), nome.title))
-            try:
+            for i in range(len(links_separados)):
+                nome = YouTube(links_separados[i])
+                print("Iniciando download da música {}/{}: {}".format(i+1, len(links_separados), nome.title))
                 musica = YouTube(links_separados[i]).streams.filter(only_audio=True).first().download('Youtube_Downloader/Músicas/Links')
                 base, ext = os.path.splitext(musica)
                 new_file = base + '.mp3'
                 os.rename(musica, new_file)
-            except():
-                print("Ocorreu um erro inesperado. ")
-        print("Concluído, sua musica se encontra: {}\Youtube_Downloader\Músicas\Links".format(os.getcwd()))
-        print("\nObrigado por usar!")
-        print('*'*100)
-        time.sleep(10)
-except():
-    print("Digite um link válido")
+                
+            print("Concluído, sua musica se encontra: {}\Youtube_Downloader\Músicas\Links".format(os.getcwd()))
+            print("Obrigado por usar!")
+            print('*'*100)
+            time.sleep(10)
+except:
+    print("Ocorreu um erro, reinicie e tente novamente!")
+    time.sleep(5)
 
 done = True
-
-
