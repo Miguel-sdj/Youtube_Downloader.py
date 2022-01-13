2a7e6e890a0c193569f0df8f5223e47dbe7bb9df
Miguel Cabral <78111674+Miguel-sdj@users.noreply.github.com>
2022-01-13 00:19:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,24 +6,34 @@ import time
 import os
 import sys
 import time
-from sentence_handling import separate_phrase
 
 is_movie = None
 is_link = None
 done = False
 
+def separate_phrase(name_file):
+    file = open(name_file, "r", encoding='utf-8')
+    lines_phrase = file.readlines()
+    list_phrases = []
+    for lines in lines_phrase:
+        lines = lines.rstrip('\n')
+        if not (lines.isnumeric()) and lines:
+            list_phrases.append(lines)
+    file.close()
+    return list_phrases
+
 def animate():
     for c in itertools.cycle(['|', '/', '-', '\\']):
         if done:
             break
-        sys.stdout.write('\rCarregando ' + c)
+        sys.stdout.write('\rLoading ' + c)
         sys.stdout.flush()
         time.sleep(0.1)
-    sys.stdout.write('\rDownload Finalizado!')
+    sys.stdout.write('\rDownload Completed!')
     if is_movie == True:
-        print("\nSeu vídeo se encontra no diretório: {}\Youtube_Downloader\Vídeos".format(os.getcwd()))
+        print("\nYour video is in the directory: {}\Youtube_Downloader\Video".format(os.getcwd()))
     else:
-        print("\nSua música se encontra no diretório: {}\Youtube_Downloader\Músicas".format(os.getcwd()))
+        print("\nYour music is in the directory: {}\Youtube_Downloader\Music".format(os.getcwd()))
     print('*'*100)
     time.sleep(20)
 
@@ -32,7 +42,7 @@ print('*'*100)
 
 link_is_valid = False
 while link_is_valid == False:
-    link_video = input("Digite o link do video ou música: ")
+    link_video = input("Enter the video or song link: ")
     link_video = str(link_video)
     if link_video[:24] == "https://www.youtube.com/":
         link_is_valid = True
@@ -48,62 +58,59 @@ while link_is_valid == False:
         link_is_valid = True
 
     else:
-        print("Link Inválido!")
+        print("Invalid Link!")
         link_is_valid = False
         is_link = False
 
-try:
-    if link_is_valid:
-        try:
-            video_error = False
-            if is_link == False:
-                video = YouTube(link_video)
-                print(f'Título do vídeo: {video.title}')
-                print(f'Duração: {video.length / 60:.2f} minutos')
-                print(f'Número de views: {video.views}\n')
-
-        except:
-            print("Vídeo indisponível!")
-            video_error = True
-
+if link_is_valid:
+    try:
+        video_error = False
         if is_link == False:
-            escolha = int(input("1 - para baixar como vídeo\n2 - para baixar a música\nEscolha: "))
-            if escolha == 1:
-                is_movie = True
-                video = video.streams.get_highest_resolution()
-                print('Para que o vídeo tenha som e imagem é necessário baixar na resolução: {}'.format(video.resolution))
-                video.download('Youtube_Downloader/Vídeos')
-                t.start()
+            video = YouTube(link_video)
+            print(f'Video Title: {video.title}')
+            print(f'Duration: {video.length / 60:.2f} minutes')
+            print(f'Number of views: {video.views}\n')
 
-            elif escolha == 2:
-                is_movie = False
-                t.start()
-                musica = YouTube(link_video).streams.filter(only_audio=True).first().download('Youtube_Downloader/Músicas')
+    except:
+        print("Video unavailable!")
+        video_error = True
+
+    if is_link == False:
+        escolha = int(input("1 - to download as video\n2 - to download as song\nChoose: "))
+        if escolha == 1:
+            is_movie = True
+            video = video.streams.get_highest_resolution()
+            print('For the video to have sound and image, it is necessary to download the resolution: {}'.format(video.resolution))
+            video.download('Youtube_Downloader/Vídeos')
+            t.start()
+
+        elif escolha == 2:
+            is_movie = False
+            t.start()
+            musica = YouTube(link_video).streams.filter(only_audio=True).first().download('Youtube_Downloader/Music')
+            base, ext = os.path.splitext(musica)
+            new_file = base + '.mp3'
+            os.rename(musica, new_file)
+
+        else:
+            print("choose a valid number")
+
+    else:
+        for i in range(len(links_separados)):
+            nome = YouTube(links_separados[i])
+            print("Starting music download {}/{}: {}".format(i+1, len(links_separados), nome.title))
+            try:
+                musica = YouTube(links_separados[i]).streams.filter(only_audio=True).first().download('Youtube_Downloader/Music/Links')
                 base, ext = os.path.splitext(musica)
                 new_file = base + '.mp3'
                 os.rename(musica, new_file)
+            except():
+                print("An unexpected error has occurred.")
+            
+        print("Completed, your music is: {}\Youtube_Downloader\Music\Links".format(os.getcwd()))
+        print("Thanks for using!")
+        print('*'*100)
+        time.sleep(10)
 
-            else:
-                print("escolha um número válido")
-
-        else:
-            for i in range(len(links_separados)):
-                nome = YouTube(links_separados[i])
-                print("Iniciando download da música {}/{}: {}".format(i+1, len(links_separados), nome.title))
-                try:
-                    musica = YouTube(links_separados[i]).streams.filter(only_audio=True).first().download('Youtube_Downloader/Músicas/Links')
-                    base, ext = os.path.splitext(musica)
-                    new_file = base + '.mp3'
-                    os.rename(musica, new_file)
-                except():
-                    print("Ocorreu um erro inesperado. ")
-                
-            print("Concluído, sua musica se encontra: {}\Youtube_Downloader\Músicas\Links".format(os.getcwd()))
-            print("Obrigado por usar!")
-            print('*'*100)
-            time.sleep(10)
-except:
-    print("Ocorreu um erro, reinicie e tente novamente!")
-    time.sleep(5)
 
 done = True
