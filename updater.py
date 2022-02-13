bbd3cc9314ea4559a91b1a1553ae18f0b812a68a
Miguel Cabral <miguelccarvalho1@gmail.com>
2022-02-13 16:00:23 -0300

    Added zip extraction after downloading

--- updater.py
+++ updater.py
@@ -22,15 +22,22 @@ links_separados = separate_phrase('version.txt')
 
 version = links_separados[0]
 name_zip = links_separados[1]
+zip_version = name_zip.split()
+
 
 myBat = open(r'temporary_update.bat','w+')
 
 name_repo = "curl -L https://github.com/Miguel-sdj/Youtube_Downloader.py/releases/download/"
 
 myBat.write(name_repo + version + name_zip)
+cwd = os.getcwd()
+myBat.write("\nmd "+ cwd + "\\novo_update")
+
+power_shell = ('\npowershell.exe -nologo -noprofile -command ' + '"' + "& { $shell = New-Object -COM Shell.Application; $target = $shell.NameSpace('" + cwd + "\\novo_update" + "')" + "; $zip = $shell.NameSpace('" + cwd + "\\" + zip_version[0] + "')" + "; $target.CopyHere($zip.Items(), 16); }")
+myBat.write(power_shell)
+
 myBat.close()
 subprocess.call([r'temporary_update.bat'])
 
 os.remove("temporary_update.bat")
 os.remove("version.txt")
-
